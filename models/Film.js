7ae1f1c1f053107dd7eed622ec08cef04c605df5
shencanet <santi.dionis@gmail.com>
2022-07-03 19:39:14 +0200

    This is a new commit for what I originally planned to be amended

---
+++ models/Film.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const filmSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    genre: {
+        type: String,
+        enum: ['terror','comedia','suspense','accion','ciencia ficcion','musical','not specified'],
+        default: 'not specified'
+    },
+    actors: {
+        type: Array,
+        required: true
+    }
+})
+
+const Film = mongoose.model('Film', filmSchema);
+
+module.exports = Film;
